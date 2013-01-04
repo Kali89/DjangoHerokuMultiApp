b1d48a7d1a36e594a98901df030195c24241d049
Kali89 <matthewsharpe3@gmail.com>
2013-01-04 23:58:54 +0000

    Added the first iteration of the users model

---
+++ users/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+class User(models.Model):
+    username = models.CharField(max_length=60)
+    created = models.DateTimeField(auto_now_add=True)
+    password = models.CharField(max_length=40)
+
+    def __unicode__(self):
+        return self.username
+
+    class Meta:
+        ordering = ('created',)
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    created = models.DateTimeField(auto_now_add=True)
+    users = models.ManyToManyField(User)
+
+    def __unicode__(self):
+        return self.title
+
+    class Meta:
+        ordering = ('created',)
+    
