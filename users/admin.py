d5a8f38d8de275c15deae54086b6e58d44b31e42
Kali89 <matthewsharpe3@gmail.com>
2013-01-05 00:38:33 +0000

    Adding relative template path and customising the admin pages

--- users/admin.py
+++ users/admin.py
@@ -1,16 +1,4 @@
 from django.contrib import admin
-from users.models import User, Post
+from users.models import User
 
-
-class PostInLine(admin.StackedInline):
-    model = Post
-    extra = 5
-
-class UserAdmin(admin.ModelAdmin):
-    fieldsets = [
-        (None,  {'fields': ['title']}),
-        ('Date information, {'fields: ['created'], 'classes': ['collapse']}),
-    ]
-    inlines = [PostInLine]
-
-admin.site.register(User, UserAdmin)
+admin.site.register(User)
