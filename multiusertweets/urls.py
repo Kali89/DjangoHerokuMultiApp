207030b78a52653cbf731fa69acce0b8e41b2b4b
Kali89 <matthewsharpe3@gmail.com>
2013-01-06 16:33:05 +0000

    Adding static_urlpatterns

--- multiusertweets/urls.py
+++ multiusertweets/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.views.generic.simple import direct_to_template
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from django.contrib import admin
 admin.autodiscover()
@@ -9,3 +10,5 @@ urlpatterns = patterns('',
     url(r'^users/', include('users.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
+
+urlpatterns += staticfiles_urlpatterns()
