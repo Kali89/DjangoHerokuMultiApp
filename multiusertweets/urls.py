f8c7c43eb8e9bc35e87c651e1e45309b92f41f3c
Kali89 <matthewsharpe3@gmail.com>
2013-01-17 00:05:56 +0000

    Added necessary imports

--- multiusertweets/urls.py
+++ multiusertweets/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.views.generic.simple import direct_to_template
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
 
 from django.contrib import admin
 admin.autodiscover()
