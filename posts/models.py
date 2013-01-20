80a7d598bf5d09f7694c7e114cdff8b757008456
Kali89 <matthewsharpe3@gmail.com>
2013-01-20 19:50:12 +0000

    Evolved the model a little bit (to include voting)

--- posts/models.py
+++ posts/models.py
@@ -6,3 +6,4 @@ class Post(models.Model):
     title = models.CharField(max_length=100)
     post = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
+    rating = models.IntegerField(default=100)
