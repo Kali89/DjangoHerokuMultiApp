3e70e275b0e95684a2ed7d89e4b8210da547dffb
Kali89 <matthewsharpe3@gmail.com>
2013-01-22 23:30:56 +0000

    Played around with the static files settings.

--- multiusertweets/settings.py
+++ multiusertweets/settings.py
@@ -64,7 +64,7 @@ STATIC_ROOT = os.path.join(PROJECT_DIR,'')
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    
+	os.path.join(STATIC_ROOT, 'static'),
 )
 
 # List of finder classes that know how to find static files in
